c6b44637c5a9bb80335a3ffcbead1ac6d77a756e
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-02-24 18:15:06 +0900

    Add: 人気の映画を表示

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,13 +1,15 @@
 import AppLayout from '@/components/Layouts/AppLayout'
 import axios from 'axios'
 import Head from 'next/head'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
-const Dashboard = () => {
+const Home = () => {
+    const [movies, setMovies] = useState([])
     useEffect(() => {
         const fetchPopularMovies = async () => {
             try {
                 const response = await axios.get('api/getPopularMovies')
+                setMovies(response.data.results)
             } catch (error) {
                 console.error(error)
             }
@@ -26,17 +28,15 @@ const Dashboard = () => {
                 <title>Laravel - Dashboard</title>
             </Head>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
+            {movies.map(movie => (
+                <img
+                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                    key={movie.id}
+                    alt={movie.title}
+                />
+            ))}
         </AppLayout>
     )
 }
 
-export default Dashboard
+export default Home
