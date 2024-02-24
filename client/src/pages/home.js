c2282d1d73755669e1f7f11c04bdf58cdf35b63e
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-02-25 00:30:04 +0900

    Add: Swiperを使って人気映画をスライドで表示

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,6 +2,8 @@ import AppLayout from '@/components/Layouts/AppLayout'
 import axios from 'axios'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
 const Home = () => {
     const [movies, setMovies] = useState([])
@@ -28,13 +30,42 @@ const Home = () => {
                 <title>Laravel - Dashboard</title>
             </Head>
 
-            {movies.map(movie => (
-                <img
-                    src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                    key={movie.id}
-                    alt={movie.title}
-                />
-            ))}
+            <Swiper
+                spaceBetween={30}
+                slidesPerView={5}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={swiper => console.log(swiper)}
+                breakpoints={{
+                    // 320px以上の画面サイズの場合
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 10,
+                    },
+                    // 480px以上の画面サイズの場合
+                    480: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    // 640px以上の画面サイズの場合
+                    640: {
+                        slidesPerView: 4,
+                        spaceBetween: 30,
+                    },
+                    // 768px以上の画面サイズの場合
+                    768: {
+                        slidesPerView: 5,
+                        spaceBetween: 40,
+                    },
+                }}>
+                {movies.map(movie => (
+                    <SwiperSlide key={movie.id}>
+                        <img
+                            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                            alt={movie.title}
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </AppLayout>
     )
 }
