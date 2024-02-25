e69c6125c1d6eca5701b83fb08accffaf59fe88b
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-02-25 19:08:24 +0900

    Style: 作品詳細画面をスタイリング

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -1,15 +1,55 @@
+import { Box, Container, Grid, Typography } from '@mui/material'
 import axios from 'axios'
 
 const Detail = ({ detail }) => {
     console.log(detail)
     return (
-        <>
-            <div>作品詳細ページ</div>
-            <h1>{detail.title}</h1>
-        </>
+        <Box
+            sx={{
+                height: { xs: 'auto', md: '70vh' },
+                position: 'relative',
+                display: 'flex',
+                alignItems: 'center',
+            }}>
+            <Box
+                sx={{
+                    backgroundImage: `url(https://image.tmdb.org/t/p/original/${detail.backdrop_path})`,
+                    position: 'absolute',
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                }}
+            />
+            <Container sx={{ zIndex: 1 }}>
+                <Grid container alignItems={'center'} color={'white'}>
+                    <Grid
+                        item
+                        md={4}
+                        sx={{ display: 'flex', justifyContent: 'center' }}>
+                        <img
+                            width={'70%'}
+                            src={`https://image.tmdb.org/t/p/original/${detail.poster_path}`}
+                            alt=""
+                        />
+                    </Grid>
+
+                    <Grid item md={8}>
+                        <Typography variant="h4" paragraph>
+                            {detail.title}
+                        </Typography>
+                        <Typography paragraph>{detail.overview}</Typography>
+                        <Typography variant="h6">
+                            公開日：{detail.release_date}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     )
 }
 
+// SSR
 export async function getServerSideProps(context) {
     const { media_type, media_id } = context.params
 
