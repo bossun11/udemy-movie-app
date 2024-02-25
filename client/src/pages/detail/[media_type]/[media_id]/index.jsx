1d22ce22125a10aee3de882461362bbd9dc79e93
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-02-25 21:33:44 +0900

    Fix: 日本語でのあらすじが取得できなかった場合は英語のあらすじを表示するように修正

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -54,11 +54,19 @@ export async function getServerSideProps(context) {
     const { media_type, media_id } = context.params
 
     try {
-        const response = await axios.get(
+        const jpResponse = await axios.get(
             `https://api.themoviedb.org/3/${media_type}/${media_id}?api_key=${process.env.TMDB_API_KEY}&language=ja-JP`,
         )
-        const fetchedData = response.data
-        return { props: { detail: fetchedData } }
+
+        let combinedData = { ...jpResponse.data }
+
+        if (!jpResponse.data.overview) {
+            const enResponse = await axios.get(
+                `https://api.themoviedb.org/3/${media_type}/${media_id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`,
+            )
+            combinedData.overview = enResponse.data.overview
+        }
+        return { props: { detail: combinedData } }
     } catch (error) {
         return { notFound: true }
     }
