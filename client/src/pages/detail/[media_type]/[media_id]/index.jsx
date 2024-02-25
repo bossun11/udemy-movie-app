344341eb64fa0831dbde96b16175945f2d58b006
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-02-25 18:23:23 +0900

    Add: SSRを使って作品情報を取得

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -1,5 +1,27 @@
-const Detail = () => {
-    return <div>作品詳細ページ</div>
+import axios from 'axios'
+
+const Detail = ({ detail }) => {
+    console.log(detail)
+    return (
+        <>
+            <div>作品詳細ページ</div>
+            <h1>{detail.title}</h1>
+        </>
+    )
+}
+
+export async function getServerSideProps(context) {
+    const { media_type, media_id } = context.params
+
+    try {
+        const response = await axios.get(
+            `https://api.themoviedb.org/3/${media_type}/${media_id}?api_key=${process.env.TMDB_API_KEY}&language=ja-JP`,
+        )
+        const fetchedData = response.data
+        return { props: { detail: fetchedData } }
+    } catch (error) {
+        return { notFound: true }
+    }
 }
 
 export default Detail
