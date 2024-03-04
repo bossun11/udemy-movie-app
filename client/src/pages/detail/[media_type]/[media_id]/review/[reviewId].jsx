284252bc3de2551a812fd38d5084a8fa2cfef2f6
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-03-04 13:51:26 +0900

    Add: レビューとコメントを取得するAPI呼び出し関数を作成

--- client/src/pages/detail/[media_type]/[media_id]/review/[reviewId].jsx
+++ client/src/pages/detail/[media_type]/[media_id]/review/[reviewId].jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import AppLayout from '@/components/Layouts/AppLayout'
+import laravelAxios from '@/lib/laravelAxios'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const ReviewDetail = () => {
-    return <div>レビュー内容</div>
+    const router = useRouter()
+    const { reviewId } = router.query
+
+    useEffect(() => {
+        const fetchReviewDetail = async () => {
+            if (!reviewId) return
+            try {
+                const response = await laravelAxios.get(
+                    `api/reviews/${reviewId}`,
+                )
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchReviewDetail()
+    }, [reviewId])
+
+    return (
+        <AppLayout
+            header={
+                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
+                    ReviewDetail
+                </h2>
+            }>
+            <Head>
+                <title>Laravel - ReviewDetail</title>
+            </Head>
+        </AppLayout>
+    )
 }
 
 export default ReviewDetail
