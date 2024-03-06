743063a6e5dd3e0dd83a903d36eba7e2cdc331ef
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-03-06 11:54:43 +0900

    Add: 取得したレビューとコメントを表示するように修正

---
+++ client/src/components/CommentList.jsx
@@ -0,0 +1,16 @@
+import { Grid } from '@mui/material'
+import Comment from './Comment'
+
+const CommentList = ({ comments }) => {
+    return (
+        <Grid container spacing={3} sx={{ mt: 2 }}>
+            {comments.map(comment => (
+                <Grid item xs={12} key={comment.id}>
+                    <Comment comment={comment} />
+                </Grid>
+            ))}
+        </Grid>
+    )
+}
+
+export default CommentList
--- client/src/pages/detail/[media_type]/[media_id]/review/[reviewId].jsx
+++ client/src/pages/detail/[media_type]/[media_id]/review/[reviewId].jsx
@@ -1,20 +1,26 @@
+import CommentList from '@/components/CommentList'
 import AppLayout from '@/components/Layouts/AppLayout'
 import laravelAxios from '@/lib/laravelAxios'
+import { Card, CardContent, Container, Rating, Typography } from '@mui/material'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const ReviewDetail = () => {
     const router = useRouter()
     const { reviewId } = router.query
+    const [review, setReview] = useState(null)
+    const [comments, setComments] = useState([])
 
     useEffect(() => {
         const fetchReviewDetail = async () => {
             if (!reviewId) return
             try {
                 const response = await laravelAxios.get(
-                    `api/reviews/${reviewId}`,
+                    `api/review/${reviewId}`,
                 )
+                setReview(response.data)
+                setComments(response.data.comments)
             } catch (error) {
                 console.log(error)
             }
@@ -32,6 +38,42 @@ const ReviewDetail = () => {
             <Head>
                 <title>Laravel - ReviewDetail</title>
             </Head>
+
+            <Container sx={{ py: 2 }}>
+                {review ? (
+                    <>
+                        {/* レビュー内容 */}
+                        <Card sx={{ minHeight: '200px' }}>
+                            <CardContent>
+                                <Typography
+                                    variant="h6"
+                                    component={'div'}
+                                    gutterBottom>
+                                    {review.user.name}
+                                </Typography>
+
+                                <Rating
+                                    name="read-only"
+                                    value={review.rating}
+                                    readOnly
+                                />
+
+                                <Typography
+                                    variant="body2"
+                                    color="textSecondary"
+                                    component="p">
+                                    {review.content}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+
+                        {/* コメント一覧 */}
+                        <CommentList comments={comments} />
+                    </>
+                ) : (
+                    <p>loading...</p>
+                )}
+            </Container>
         </AppLayout>
     )
 }
