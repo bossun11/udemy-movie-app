c60a091e21c42bae7102d828c8a54bc1b736fcb3
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2024-03-13 10:51:23 +0900

    Add: お気に入り登録ボタンを追加

--- client/src/pages/detail/[media_type]/[media_id]/index.jsx
+++ client/src/pages/detail/[media_type]/[media_id]/index.jsx
@@ -9,6 +9,7 @@ import {
     Container,
     Fab,
     Grid,
+    IconButton,
     Modal,
     Rating,
     TextareaAutosize,
@@ -17,6 +18,7 @@ import {
 } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import StarIcon from '@mui/icons-material/Star'
+import FavoriteIcon from '@mui/icons-material/Favorite'
 import axios from 'axios'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
@@ -138,6 +140,18 @@ const Detail = ({ detail, media_type, media_id }) => {
         }
     }
 
+    const handleToggleFavorite = async () => {
+        try {
+            const response = await laravelAxios.post(`api/favorites`, {
+                media_type,
+                media_id,
+            })
+            console.log(response.data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     useEffect(() => {
         const fetchReviews = async () => {
             try {
@@ -199,6 +213,15 @@ const Detail = ({ detail, media_type, media_id }) => {
                             <Typography variant="h4" paragraph>
                                 {detail.title || detail.name}
                             </Typography>
+
+                            <IconButton
+                                style={{
+                                    color: 'white',
+                                    background: '#0d253f',
+                                }}>
+                                <FavoriteIcon onClick={handleToggleFavorite} />
+                            </IconButton>
+
                             <Typography paragraph>{detail.overview}</Typography>
 
                             <Box
